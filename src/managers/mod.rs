78dbf9ca590f92f0df3454b9dc81f1fae6e25ef0
Mateusz Giza <matgizq@gmail.com>
2018-10-18 21:23:14 +0200

    Add project with Resource Managers

---
+++ src/managers/mod.rs
@@ -0,0 +1,8 @@
+use sdl2::render::{ Texture, TextureCreator };
+use sdl2::ttf::{ Font, Sdl2TtfContext };
+
+mod resource_manager;
+use self::resource_manager::{ ResourceManager, FontDetails };
+
+pub type TextureManager<'l, T> = ResourceManager<'l, String, Texture<'l>, TextureCreator<T>>;
+pub type FontManager<'l> = ResourceManager<'l, FontDetails, Font<'l, 'static>, Sdl2TtfContext>;
