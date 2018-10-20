24a99ce4f198acab38df995bb91c09761a481bea
Mateusz Giza <matgizq@gmail.com>
2018-10-21 01:29:20 +0200

    make resource_manager module public

--- src/managers/mod.rs
+++ src/managers/mod.rs
@@ -1,7 +1,7 @@
 use sdl2::render::{ Texture, TextureCreator };
 use sdl2::ttf::{ Font, Sdl2TtfContext };
 
-mod resource_manager;
+pub mod resource_manager;
 use self::resource_manager::{ ResourceManager, FontDetails };
 
 pub type TextureManager<'l, T> = ResourceManager<'l, String, Texture<'l>, TextureCreator<T>>;
