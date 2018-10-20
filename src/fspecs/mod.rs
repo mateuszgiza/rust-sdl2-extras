4366f3d73658a30f7d99f659c86638e06c85aef5
Mateusz Giza <matgizq@gmail.com>
2018-10-21 01:51:31 +0200

    Add doc for fspecs feature

--- src/fspecs/mod.rs
+++ src/fspecs/mod.rs
@@ -1,2 +1,20 @@
+//!
+//! Extras for Specs crate (ECS framework)
+//! 
+//! To use this you'll need to build with the feature `fspecs`:
+//! 
+//! ```bash
+//! $ cargo build --features "fspecs"
+//! ```
+//! 
+//! or add the following line in Cargo.toml file inside your crate:
+//! 
+//! ```toml
+//! [dependencies.sdl2-extras]
+//! version = ...
+//! default-features = false
+//! features = ["fspecs"]
+//! ```
+
 mod extensions;
 pub use self::extensions::WorldExt;
