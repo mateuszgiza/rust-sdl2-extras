224b043bb4f718d543617fbd783b62f60c382ce8
Mateusz Giza <matgizq@gmail.com>
2018-10-21 03:31:11 +0200

    Add doc to lib.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! # Rust-SDL2-Extras
+//! 
+//! Additional functionalities and helpers
+//! which were not included in the original 
+//! crate (but were included in examples)
+//! or can help with some bottlenecks 
+//! and improve code readability and quality.
+//! 
+
 extern crate sdl2;
 
 #[cfg(feature = "fspecs")]
