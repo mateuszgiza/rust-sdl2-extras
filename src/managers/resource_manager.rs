1e72c8e5d37c9838d9ac790bb3f658bf1914d3fa
Mateusz Giza <matgizq@gmail.com>
2018-10-21 03:44:47 +0200

    Rename ResourceManager generic types definition to be more readable

--- src/managers/resource_manager.rs
+++ src/managers/resource_manager.rs
@@ -4,27 +4,27 @@ use std::hash::Hash;
 use std::rc::Rc;
 
 // Generic trait to Load any Resource Kind
-pub trait ResourceLoader<'l, R> {
+pub trait ResourceLoader<'l, TResult> {
     type Args: ?Sized;
-    fn load(&'l self, data: &Self::Args) -> Result<R, String>;
+    fn load(&'l self, data: &Self::Args) -> Result<TResult, String>;
 }
 
 // Generic struct to cache any resource loaded by a ResourceLoader
-pub struct ResourceManager<'l, K, R, L>
+pub struct ResourceManager<'l, TKey, TResult, TLoader>
 where
-    K: Hash + Eq,
-    L: 'l + ResourceLoader<'l, R>,
+    TKey: Hash + Eq,
+    TLoader: 'l + ResourceLoader<'l, TResult>,
 {
-    loader: &'l L,
-    cache: HashMap<K, Rc<R>>,
+    loader: &'l TLoader,
+    cache: HashMap<TKey, Rc<TResult>>,
 }
 
-impl<'l, K, R, L> ResourceManager<'l, K, R, L>
+impl<'l, TKey, TResult, TLoader> ResourceManager<'l, TKey, TResult, TLoader>
 where
-    K: Hash + Eq,
-    L: ResourceLoader<'l, R>,
+    TKey: Hash + Eq,
+    TLoader: ResourceLoader<'l, TResult>,
 {
-    pub fn new(loader: &'l L) -> Self {
+    pub fn new(loader: &'l TLoader) -> Self {
         ResourceManager {
             cache: HashMap::new(),
             loader: loader,
@@ -33,11 +33,11 @@ where
 
     // Generics magic to allow a HashMap to use String as a key
     // while allowing it to use &str for gets
-    pub fn load<D>(&mut self, details: &D) -> Result<Rc<R>, String>
+    pub fn load<D>(&mut self, details: &D) -> Result<Rc<TResult>, String>
     where
-        L: ResourceLoader<'l, R, Args = D>,
+        TLoader: ResourceLoader<'l, TResult, Args = D>,
         D: Eq + Hash + ?Sized,
-        K: Borrow<D> + for<'a> From<&'a D>,
+        TKey: Borrow<D> + for<'a> From<&'a D>,
     {
         self.cache.get(details).cloned().map_or_else(
             || {
