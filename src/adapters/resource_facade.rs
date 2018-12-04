ab00e127bddab22d0315262c283c02e324ce9070
Mateusz Giza <matgizq@gmail.com>
2018-12-04 15:26:43 +0100

    Wrap ResourceFacade fields in Option<> struct

--- src/adapters/resource_facade.rs
+++ src/adapters/resource_facade.rs
@@ -6,9 +6,20 @@ use sdl2::pixels::Color;
 use sdl2::ttf::Sdl2TtfContext;
 
 pub struct ResourceFacade<'l> {
-    font_manager: FontManager<'l>,
-    texture_manager: TextureManager<'l, WindowContext>,
-    texture_creator: &'l TextureCreator<WindowContext>
+    font_manager: Option<FontManager<'l>>,
+    texture_manager: Option<TextureManager<'l, WindowContext>>,
+    texture_creator: Option<&'l TextureCreator<WindowContext>>
+}
+
+unsafe impl<'l> Send for ResourceFacade<'l> {}
+unsafe impl<'l> Sync for ResourceFacade<'l> {}
+
+impl<'l> Default for ResourceFacade<'l> {
+    fn default() -> Self { ResourceFacade{
+        font_manager: None,
+        texture_manager: None,
+        texture_creator: None
+    } }
 }
 
 impl<'l> ResourceFacade<'l> {
@@ -17,17 +28,17 @@ impl<'l> ResourceFacade<'l> {
         let texture_manager = TextureManager::new(&texture_creator);
 
         ResourceFacade {
-            font_manager,
-            texture_manager,
-            texture_creator
+            font_manager: Some(font_manager),
+            texture_manager: Some(texture_manager),
+            texture_creator: Some(texture_creator)
         }
     }
 
     pub fn build_text<'a>(&'a mut self, text: &str, font_details: &FontDetails, color: &Color) -> TextTexture<'a> {
-        let font = self.font_manager.load(font_details).unwrap();
+        let font = self.font_manager.as_mut().unwrap().load(font_details).unwrap();
         let text_render = font.render(text);
         let text_surface = text_render.solid(*color).unwrap();
-        let text_texture = self.texture_creator.create_texture_from_surface(text_surface).unwrap();
+        let text_texture = self.texture_creator.unwrap().create_texture_from_surface(text_surface).unwrap();
         let text_query = text_texture.query();
 
         return TextTexture::new(text_texture, text_query);
